caf2f55ddaa0fe63570ab2592510f81ebaf3e2d1
Fynmar <mail@fynmar.de>
2022-10-05 19:27:43 +0200

    BaseSpeed can now be updated at runtime

--- code/NPC/Types/StandardNPC.cs
+++ code/NPC/Types/StandardNPC.cs
@@ -7,7 +7,7 @@ public partial class Peasant : BaseNPC
 {
 	public override string NPCName => "Peasant";
 	public override float BaseHealth => 15;
-	public override float BaseSpeed => 15;
+	public override float BaseSpeed { get; set; } = 15;
 	public override string BaseModel => "models/citizen/citizen.vmdl";
 	public override int[] MinMaxCashReward => new int[] { 1, 15 };
 	public override int[] MinMaxEXPReward => new int[] { 1, 5 };
@@ -24,7 +24,7 @@ public partial class Zombie : BaseNPC
 {
 	public override string NPCName => "Zombie";
 	public override float BaseHealth => 65;
-	public override float BaseSpeed => 12.5f;
+	public override float BaseSpeed { get; set; } = 12.5f;
 	public override string BaseModel => "models/citizen/citizen.vmdl";
 	public override int[] MinMaxCashReward => new int[] { 6, 24 };
 	public override int[] MinMaxEXPReward => new int[] { 4, 13 };
