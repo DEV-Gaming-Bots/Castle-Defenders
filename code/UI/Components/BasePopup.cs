25c16cb71ad5f0c2532b24551835758cd4ee0e87
AlexVeeBee <alex200.veebee@gmail.com>
2022-12-11 16:46:17 +0000

    Import Popup

---
+++ code/UI/Components/BasePopup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace Components.popup
+{
+	public enum PopupVertical
+	{
+		Top,
+		Center,
+		Bottom,
+	};
+
+	public enum PopupHorizontal
+	{
+		Left,
+		Center,
+		Right
+	}
+	
+	public class WindowPopup : Panel
+	{
+		public string SCSS;
+
+		public Panel body;
+		public Label title;
+		public Panel Header;
+		public Panel content;
+		public Panel footer;
+		// vertical and horizontal 
+		public PopupVertical _Vertical = PopupVertical.Center;
+		public PopupHorizontal _Horizontal = PopupHorizontal.Center;
+
+		public WindowPopup( string title, PopupVertical Vertical = PopupVertical.Center, PopupHorizontal Horizontal = PopupHorizontal.Center )
+		{
+			_Vertical = Vertical;
+			_Horizontal = Horizontal;
+
+			StyleSheet.Load( SCSS != null ? SCSS : "UI/Components/BasePopup.scss" );
+
+			AddClass( "popup-body" );
+			
+			/* check if vertical is top center or bottom */
+			switch( Vertical )
+			{
+				case PopupVertical.Top: AddClass( "v-popup-top" ); break;
+				case PopupVertical.Center: AddClass( "v-popup-center" ); break;
+				case PopupVertical.Bottom: AddClass( "v-popup-bottom" ); break;
+				default: AddClass( "v-popup-center" ); Log.Warning( "No Vertical Alignment" ); break;
+			}
+
+			/* check if horizontal is left center or right */
+			switch ( Horizontal )
+			{
+				case PopupHorizontal.Left: AddClass( "h-popup-left" ); break;
+				case PopupHorizontal.Center: AddClass( "h-popup-center " ); break;
+				case PopupHorizontal.Right: AddClass( "h-popup-right " ); break;
+				default: AddClass( "h-popup-center" ); Log.Warning( "No Horizontal Alignment" ); break;
+			}
+
+
+
+			body = Add.Panel( "body" );
+			Header = body.Add.Panel( "header" );
+			this.title = Header.Add.Label( title, "title" );
+			content = body.Add.Panel( "content" );
+			footer = body.Add.Panel( "footer" );
+		}
+	}
+}
